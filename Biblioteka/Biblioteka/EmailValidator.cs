537ddc328843a506b6d2ba9b362865fc0c387747
AidasSin <aidassinicinas@gmail.com>
2021-09-12 17:02:10 +0300

    Created the project

---
+++ Biblioteka/Biblioteka/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteka
+{
+    class EmailValidator
+    {
+        public bool IsEmailValid(string email)
+        {
+            return true;
+        }
+    }
+}
---
+++ Biblioteka/Biblioteka/PhoneValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteka
+{
+    class PhoneValidator
+    {
+        public bool IsPhoneNumberValid(string phoneNumber)
+        {
+            return true;
+        }
+    }
+}
