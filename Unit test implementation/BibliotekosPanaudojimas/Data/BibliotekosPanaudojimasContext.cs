08e5a5c35aa1f1525048440049380da6a8181609
AidasSin <aidassinicinas@gmail.com>
2021-11-05 17:18:02 +0200

    initial project creation, minor additions

---
+++ Unit test implementation/BibliotekosPanaudojimas/Data/BibliotekosPanaudojimasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BibliotekosPanaudojimas.Models;
+
+namespace BibliotekosPanaudojimas.Data
+{
+    public class BibliotekosPanaudojimasContext : DbContext
+    {
+        public BibliotekosPanaudojimasContext (DbContextOptions<BibliotekosPanaudojimasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BibliotekosPanaudojimas.Models.User> User { get; set; }
+    }
+}
