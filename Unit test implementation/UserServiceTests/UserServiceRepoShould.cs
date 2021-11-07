0472f263dd4580bd6734762445ddaed89759d833
AidasSin <aidassinicinas@gmail.com>
2021-11-07 18:58:44 +0200

    Removed test project

--- Unit test implementation/UserServiceTests/UserServiceRepoShould.cs
+++ Unit test implementation/UserServiceTests/UserServiceRepoShould.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using _1_3_Bibliotekos_Panaudojimas.Entities;
+
 
 namespace UserServiceTests
 {
@@ -9,7 +11,7 @@ namespace UserServiceTests
         [TestMethod]
         public void CreateNewUser()
         {
-
+            var user = new UserEntity
         }
     }
 }
