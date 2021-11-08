5e514e1378836af7c2bfa8b6c7ac7465e0921ca9
AidasSin <aidassinicinas@gmail.com>
2021-11-08 16:57:40 +0200

    Module unit tests.

--- Unit test implementation/BibliotekosPanaudojimasTests/UserServiceRepoShould.cs
+++ Unit test implementation/BibliotekosPanaudojimasTests/UserServiceRepoShould.cs
@@ -7,33 +7,89 @@ using _1_3_Bibliotekos_Panaudojimas;
 using System.Threading;
 using System.Collections.Generic;
 using Moq;
+using System.Linq;
 
 namespace BibliotekosPanaudojimasTests
 {
     public class UserServiceRepoShould
     {
+
         [Fact]
-        public async Task Test1Async()
+        public void GetUserById()
         {
-            var userInMemoryDB = new List<UserEntity>
-            {
-                new UserEntity()
-                {
-                    Id = 1, Name = "Testas", LastName = "Testainis",
-                    Email = "testainis@pastainis.lt", Phone = "+37061111111", Password = "Slaptazodis3000!"
-                },
-                new UserEntity()
-                {
-                    Id = 2, Name = "Kitastestas", LastName = "Kitastestainis",
-                    Email = "testainis@pastainis2.lt", Phone = "+37061111112", Password = "Slaptazodis3002!"
-                },
-                new UserEntity()
-                {
-                    Id = 3, Name = "Darkitastestas", LastName = "Darkitastestainis",
-                    Email = "dartestainis@pastainis2.lt", Phone = "+37061111113", Password = "Slaptazodis3003!"
-                }
-            };
+            var _cancellation = new CancellationToken();
+
             var repo = new Mock<IUserServiceRepository>();
+
+            repo.Setup(x => x.GetByIdAsync(1, _cancellation))
+                .ReturnsAsync(new UserEntity
+                {
+                    Id = 1,
+                    Name = "Testas",
+                    LastName = "Testainis",
+                    Email = "testainis@pastainis.lt",
+                    Phone = "+37061111111",
+                    Password = "Slaptazodis3000!"
+                });
+
+            var userRepo = repo.Object;
+
+            var testUser = userRepo.GetByIdAsync(1, _cancellation);
+
+            Assert.Equal(1, testUser.Id);
+            Assert.NotEqual(2, testUser.Id);
+
+        }
+        [Fact]
+        public void DeleteUserById()
+        {
+            var _cancellation = new CancellationToken();
+
+            var repo = new Mock<IUserServiceRepository>();
+
+            var user = new UserEntity
+            {
+                Id = 1,
+                Name = "Testas",
+                LastName = "Testainis",
+                Email = "testainis@pastainis.lt",
+                Phone = "+37061111111",
+                Password = "Slaptazodis3000!"
+            };
+
+            repo.Setup(r => r.DeleteUserAsync(It.IsAny<int>(), _cancellation));
+
+            var userRepo = repo.Object;
+
+            userRepo.DeleteUserAsync(1, _cancellation);
+
+            repo.Verify(r => r.DeleteUserAsync(1, _cancellation));
+
+        }
+        [Fact]
+        public void UpdateUser()
+        {
+            var _cancellation = new CancellationToken();
+
+            var repo = new Mock<IUserServiceRepository>();
+
+            var user = new UserEntity
+            {
+                Id = 1,
+                Name = "Testas",
+                LastName = "Testainis",
+                Email = "testainis@pastainis.lt",
+                Phone = "+37061111111",
+                Password = "Slaptazodis3000!"
+            };
+
+            repo.Setup(r => r.EditUserInfoAsync(It.IsAny<UserEntity>(), _cancellation));
+
+            var userRepo = repo.Object;
+
+            userRepo.EditUserInfoAsync(user, _cancellation);
+
+            repo.Verify(r => r.EditUserInfoAsync(user, _cancellation));
 
         }
     }
